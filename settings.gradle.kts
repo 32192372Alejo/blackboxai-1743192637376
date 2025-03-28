5fdd59c9297bedb9ee3b7ce78e7bddc53014c39b
32192372Alejo <alejo290816@gmail.com>
2025-03-28 16:25:00 -0500

    Add settings.gradle.kts 1743196977328

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
-rootProject.name = "InterviewSimulator"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "InterviewSimulator"
 include(":app")
